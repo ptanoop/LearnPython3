3af22ac271981aa499fc623f6e58a22939f6016d
ptanoop <ptanoop@gmail.com>
2014-06-24 16:04:37 +0400

    simple clock

---
+++ simple_clock.py
@@ -0,0 +1,34 @@
+# Keyboard Interruption in python
+"""
+
+	Author	:	ANOOP P T
+	Date	:	24/06/2014
+ 
+	Problem	:	Have the user enter a number and
+	                find all Prime Factors (if there are any) and
+	                display them.
+	
+	
+"""
+
+import msvcrt
+import os
+from datetime import datetime
+import time
+
+while True:
+    if msvcrt.kbhit():
+        if ord(msvcrt.getch()) == 27:
+            break
+    os.system('cls')
+    print(datetime.now().strftime('%H:%M:%S'))
+    time.sleep(1)
+    
+##while 1:
+##    print('Testing..')
+##    # body of the loop ...
+##    if msvcrt.kbhit():
+##        os.system('cls')
+##        print(ord(msvcrt.getch()))
+##        if ord(msvcrt.getch()) == 13:
+##            break
